d768b44e38411369918903417c0d6c1ce624510d
aumitleon <aumitleon1@gmail.com>
2018-08-31 22:46:28 -0400

    simple refactor

--- src/converter.py
+++ src/converter.py
@@ -6,6 +6,18 @@ def link_md_to_html(md_link):
     url_link = phrase_split[1][1:-1]
     link_html = "<a href=" + url_link + ">" + text_link + "</a>"
     return link_html
+
+def list_md_to_html(matches, text):
+    bullet_html = "\t\t<ul>\n"
+    for match in matches:
+        if match[0] == "*":
+            if len(matches) > 1:
+                bullet_html += "\t\t\t<li>" + inner_tags(matches) + "</li>\n"
+            else: 
+                bullet_html += "\t\t\t<li>" + text[0] + "</li>\n"
+    bullet_html += "\t\t</ul>\n"
+    return bullet_html
+
     
 def inner_tags(matches):
     for match in matches:
@@ -20,23 +32,33 @@ def inner_tags(matches):
 def main(args):
     # matches headers, bullet lists, and links
     pattern = re.compile('([#*]+\s|\w*\[\w*\]\(\w*\))')
+    text = re.compile('\w')
     html_doc = open(args.output, "a")
-
+    html_doc.write("<!DOCTYPE html>\n")
+    html_doc.write("<html>\n\t<head>\n\t</head>\n\t<body>\n")
     with open(args.input) as f:
         content = f.readlines()
         for line in content:
             matches = pattern.findall(line)
+            text_matches = text.findall(line)
             if matches:
                 # link
                 if matches[0][0] == "[":
-                    html_doc.write(link_md_to_html(matches[0]) + "\n")
+                    html_doc.write("\t\t" + link_md_to_html(matches[0]) + "\n")
                 # bullet list
                 if matches[0][0] == "*":
-                    bullet_html = "<ul>\n\t <li>" + inner_tags(matches) + "</li>\n</ul>\n"
-                    html_doc.write(bullet_html)
+                    html_doc.write(list_md_to_html(matches, text_matches))
                 if matches[0][0] == "#":
-                    header_html = "<h1>" + inner_tags(matches) + "</h1>\n"
+                    header_size = 0
+                    for char in matches[0]:
+                        if char == "#":
+                            header_size += 1
+                    if len(matches) > 1:
+                        header_html = "\t\t<h" + str(header_size) + ">" + inner_tags(matches) + "</h" + str(header_size) + ">\n"
+                    else:
+                        header_html = "\t\t<h" + str(header_size) + ">" + line.split()[1] + "</h" + str(header_size) + ">\n"
                     html_doc.write(header_html)
+    html_doc.write("\t</body>\n</html>")
 
 if __name__ == '__main__':
     main()
