2f4ebfea571c537a4b75c0bf43f5253b8054b8f5
aumitleon <aumitleon1@gmail.com>
2018-08-31 22:00:52 -0400

    style(structure): refactor structure

--- src/converter.py
+++ src/converter.py
@@ -1,55 +1,43 @@
 import re
 
-pattern = re.compile('([#*]+\s|\w*\[\w*\]\(\w*\))')
+def link_md_to_html(md_link):
+    phrase_split = md_link.split("]")
+    text_link = phrase_split[0][1:]
+    url_link = phrase_split[1][1:-1]
+    link_html = "<a href=" + url_link + ">" + text_link + "</a>"
+    return link_html
+    
+def inner_tags(matches):
+    for match in matches:
+        # skip first pattern match
+        if matches.index(match) == 0:
+            continue 
+        # could be pulled out to a different function
+        if match[0] == "[":
+            inner_html = link_md_to_html(match)
+    return inner_html
 
-md_html_mapping = {}
-md_html_mapping["## "] = "<h2>"
-html_closing_tags = {}
-html_closing_tags["<h2>"] = "</h2>"
+def main(args):
+    # matches headers, bullet lists, and links
+    pattern = re.compile('([#*]+\s|\w*\[\w*\]\(\w*\))')
+    html_doc = open(args.output, "a")
 
-html_doc = open("converted.html", "a")
-filename = "test.md"
-tags = []
-with open(filename) as f:
-    content = f.readlines()
-    for line in content:
-        match = pattern.findall(line)
-        print line
-        print match
-        if match:
-            for phrase in match:
+    with open(args.input) as f:
+        content = f.readlines()
+        for line in content:
+            matches = pattern.findall(line)
+            if matches:
                 # link
-                print phrase
-                if phrase[0] == "[":
-                    phrase_split = phrase.split("]")
-                    text_link = phrase_split[0][1:]
-                    url_link = phrase_split[1][1:-1]
-                    link_html = "<a href=" + url_link + ">" + text_link + "</a>\n"
-                    html_doc.write(link_html)
+                if matches[0][0] == "[":
+                    html_doc.write(link_md_to_html(matches[0]) + "\n")
                 # bullet list
-                if phrase[0] == "*":
-                    
-                    bullet_html = "<ul>\n\t <li>" + phrase[0][1:] + "</li>\n</ul>\n"
+                if matches[0][0] == "*":
+                    bullet_html = "<ul>\n\t <li>" + inner_tags(matches) + "</li>\n</ul>\n"
                     html_doc.write(bullet_html)
-                # list
-                #elif phrase[0] == "*":
-                 #   bullet_text = phrase.split()
-                    
+                if matches[0][0] == "#":
+                    header_html = "<h1>" + inner_tags(matches) + "</h1>\n"
+                    html_doc.write(header_html)
 
-
-
-
-    #print content
-    """ for sentence in content:
-        words = sentence.split()
-        print words
-        print sentence
-        if words[0] in md_html_mapping:
-            #print md_html_mapping[words[0]] + sentence + html_closing_tags[md_html_mapping[words[0]]]
-            html_doc.write(md_html_mapping[words[0]] + ''.join(words[1:]) + html_closing_tags[md_html_mapping[words[0]]] + "\n") """
-  
-""" 
-#[**](**)
-pattern = re.compile('([#*]+\s|\w*\[\w*\]\(\w*\))')
-m = pattern.match("* lol [lol](lol)")
-print p.findall("* lol [lol](lol)") """
+if __name__ == '__main__':
+    main()
+    
