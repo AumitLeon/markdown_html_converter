a46ab2358b4902ddd39092091da5f096e90ebd29
aumitleon <aumitleon1@gmail.com>
2019-12-17 15:21:16 -0500

    test: config test script

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,19 @@
+# Exit with non-zero status if any intermediate step fails
+set -e
+
 echo "Conducting tests..."
-echo "Using correct version of python-semantic-release..."
+
+echo "Check that md-to-html is installed"
+md-to-html -h 
+
+echo "Convert test markdown file to html"
+md-to-html --input test.md
+
+echo "Make sure that a converted.html was created"
+test -e converted.html
+
+test -e sdfsd
+
+echo "Make sure files are the same"
+diff baseline.html converted.html
+
